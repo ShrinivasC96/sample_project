aa9c5d720e241fce0ba522b4fffa6ba3678af236
ShrinivasC96 <150146654+ShrinivasC96@users.noreply.github.com>
2024-05-27 17:47:33 +0530

    Added second variation of sum of odd numbers in main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ sum_of_odd = 0
 for i in range(1,50,2):
     sum_of_odd += i
 print("The sum of odd number from 1 to 50 is: ", sum_of_odd)    
+
+#2
+sum_of_odd = sum(range(1,50,2))
+print("The sum of odd number from 1 to 50 is: ", sum_of_odd)
