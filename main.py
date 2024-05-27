e548c0dd89656b18f9f2df850d879584c80ed1dc
shrinivas <shrinivas.chavan7387@gmail.com>
2024-05-27 17:43:33 +0530

    Sum program added in main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Program to calculate sum of odd number from 1 to 50
+'''
+
+sum_of_odd = 0
+
+for i in range(1,50,2):
+    sum_of_odd += i
+print("The sum of odd number from 1 to 50 is: ", sum_of_odd)    
